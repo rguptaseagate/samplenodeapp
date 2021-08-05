9a300c7d8ad0d1070813db11f94e9c37e08e91e1
Rohan Gupta <75747077+rggitops@users.noreply.github.com>
2021-08-05 13:04:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+ 
+app.listen(3010, function () {
+  console.log("listening on 3010");
+});
+ 
+app.get("/", (req, res) => {
+  res.send("Users Shown");
+});
+ 
+app.get("/delete", (req, res) => {
+  res.send("Delete User");
+});
+ 
+app.get("/update", (req, res) => {
+  res.send("Update User");
+});
+ 
+app.get("/insert", (req, res) => {
+  res.send("Insert User");
+});
+
